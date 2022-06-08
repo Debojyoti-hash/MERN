7f3a93465d3f5dee7877f21f6bdef46fbce2dd0d
Debojyoti <85504189+Debojyoti-hash@users.noreply.github.com>
2022-06-08 19:56:02 +0530

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const hbs = require("hbs");
 require("./db/connect");
 
 const port = process.env.PORT || 3000;
 
-const static_path = path.join(__dirname, "../public");
-app.use(express.static(static_path))
+//const static_path = path.join(__dirname, "../views");
+const template_path = path.join(__dirname, "../templates/views");
+const partial_path = path.join(__dirname, "../templates/partials");
+
+app.use(express.static(template_path))
+
+app.set("view engine", "hbs");
+hbs.registerPartials(partial_path);
+app.set("views", template_path);
 
 app.get("/",(req, res) => {
-    res.send("Hello World")
+    res.render("index")
 })
 
 app.listen(port, () => {
